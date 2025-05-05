0f353780604f69d0f2e72c5f30a8730367a4d9e9
sevdenurozcan <sevdenurozcan0@gmail.com>
2025-05-05 20:42:05 +0300

    Add files via upload

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,9 @@ import { IoMdMail } from "react-icons/io";
 import { FaPhone } from "react-icons/fa6";
 
 
+import {Quicksand} from 'next/font/google';
+export const quicksand = Quicksand({ subsets: ['latin'], weight: ['400'] });
+
 export const metadata = {
   title: "SevdesThings - Handmade Clothing",
   description: "Crochet and Knitting Patterns",
@@ -14,11 +17,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={quicksand.className}>
       
       <body className="bg-bg-color ">
                  
-          <nav className="bg-hd-bg-color sticky top-0 z-100">
+          <nav className="bg-hd-bg-color ">
             <ul className="flex flex-row flex-start gap-2 text-white">
               <li>
                 <Link href="https://www.instagram.com/sevdesthings/"> <FaInstagram className="text-2xl m-1 ml-3" /> </Link>
@@ -31,13 +34,33 @@ export default function RootLayout({ children }) {
               </li>
             </ul>
             
-             <div className="bg-border-color w-full h-0.5 hdbr">
-
-            </div>
+        
           </nav>
   
 
         {children}
+        <nav>
+        <ul className="flex flex-row justify-center gap-8 mb-4">
+            <li className=' hover:text-hd-color'>
+              <Link href="/"> Home </Link>
+            </li>
+            <li className=' hover:text-hd-color'>
+              <Link href="/patterns"> Patterns </Link>
+            </li>
+            <li className=' hover:text-hd-color'>
+              <Link href="/stores"> Stores </Link>
+            </li>
+            <li className=' hover:text-hd-color'>
+              <Link href="/blog"> Blog </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <footer className='text-center text-hd-color font-extralight text-sm mb-1'>
+        © 2025 Sevde Nur Özcan. All rights reserved.
+      </footer>
+
+
       </body>
     </html>
   );
